da60a172ef17d144043e7fdcaa7fb9c300f95fab
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-09-12 08:10:23 -0600

    s

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,47 @@
 document.querySelector('#currentyear').textContent = new Date().getFullYear();
 document.querySelector('#lastmodified').textContent = document.lastModified;
+
+const courses = [
+    {
+        subject: "CSE",
+        number: 110,
+        title: "Introduction to Programming",
+        credits: 2,
+        completed: false
+    },
+    {
+        subject: "WDD",
+        number: 130,
+        title: "Web Fundamentals",
+        credits: 2,
+        completed: false
+    },
+    {
+        subject: "CSE",
+        number: 111,
+        title: "Programming with Functions",
+        credits: 2,
+        completed: false
+    },
+    {
+        subject: "CSE",
+        number: 210,
+        title: "Programming with Classes",
+        credits: 2,
+        completed: false
+    },
+    {
+        subject: "WDD",
+        number: 131,
+        title: "Dynamic Web Fundamentals",
+        credits: 2,
+        completed: false
+    },
+    {
+        subject: "WDD",
+        number: 231,
+        title: "Web Frontend Development I",
+        credits: 2,
+        completed: false
+    }
+];
