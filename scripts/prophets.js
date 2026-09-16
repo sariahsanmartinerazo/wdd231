2146cd16e46ab3656ed2e2f81d919afe60b271ed
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-09-16 10:33:36 -0600

    s

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,11 +1,8 @@
-const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const url = 'https://byui-cse-github.io/cse-ww-program/data/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
-
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets); //
+    displayProphets(data.prophets);
 }
-
-getProphetData();
+getProphetData(); 
