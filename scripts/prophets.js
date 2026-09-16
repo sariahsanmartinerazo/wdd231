b7d6f344aa7c3291da0f047d1870d24d0262d9d7
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-09-16 13:08:23 -0600

    s

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -3,6 +3,26 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
+    console.table(data.prophets);
     displayProphets(data.prophets);
+
 }
 getProphetData(); 
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+//build the h2 content
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440'); 
+//append the section(card) with the created elements
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}   
