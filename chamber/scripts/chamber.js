b930fcf706b22e22c8e42a5120a52adfe9783eab
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-09-23 18:09:20 -0600

    s

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -187,7 +187,9 @@ fetch(forecastURL)
     .then(data => {
         const forecast = data.list;
 
-        console.log(forecast[0]);
+        console.log(forecast[0].dt_txt);
+        console.log(forecast[0].main.temp);
+        console.log(forecast[0].weather[0].description);
         console.log(forecast[8]);
         console.log(forecast[16]);
     });
